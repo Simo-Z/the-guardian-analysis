2dbf168a4e6d43619e9006e019cebb551b2269fd
Simo-Z <mail@simozilling.de>
2022-06-03 16:33:58 +0200

    add basic streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,41 +1,14 @@
+from bz2 import compress
 import altair as alt
 import pandas as pd
 import streamlit as st
-from vega_datasets import data
+# from st_aggrid import AgGrid
 
-@st.experimental_memo
-def get_data():
-    source = data.stocks()
-    source = source[source.date.gt("2004-01-01")]
-    return source
+'''
+# The Guardian Analysis
+#### What role do climate related news play on The Guardian?
+'''
 
-source = get_data()
+articles = pd.read_csv("data/clean/2022_clean.csv.zip", compression="zip")
 
-# Original time series chart. Omitted `get_chart` for clarity
-chart = get_chart(source)
-
-# Input annotations
-ANNOTATIONS = [
-    ("Mar 01, 2008", "Pretty good day for GOOG"),
-    ("Dec 01, 2007", "Something's going wrong for GOOG & AAPL"),
-    ("Nov 01, 2008", "Market starts again thanks to..."),
-    ("Dec 01, 2009", "Small crash for GOOG after..."),
-]
-
-# Create a chart with annotations
-annotations_df = pd.DataFrame(ANNOTATIONS, columns=["date", "event"])
-annotations_df.date = pd.to_datetime(annotations_df.date)
-annotations_df["y"] = 0
-annotation_layer = (
-    alt.Chart(annotations_df)
-    .mark_text(size=15, text="⬇", dx=-12, dy=-8, align="center")
-    .encode(
-        x="date:T",
-        y=alt.Y("y:Q"),
-        tooltip=["event"],
-    )
-    .interactive()
-)
-
-# Display both charts together
-st.altair_chart((chart + annotation_layer).interactive(), use_container_width=True)
+st.write(articles)
